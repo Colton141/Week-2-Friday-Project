79293b492f22b6ba65f27f97f203af590009e96c
colton lacey <coltnutmiles14@gmail.com>
2019-03-29 15:45:46 -0700

    fix js for button to show results of survey

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,33 @@
-(document).ready(function(){
-  ("button#enter").click(funtion(event){
-    var
+$(document).ready(function(){
+  $("button#enter").click(function(event){
+    var question1 = $("#question1").val();
+    var question2 = $("#question2").val();
+    var question3 = $("#question3").val();
+    var question4 = $("#question4").val();
+    var question5 = $("#question5").val();
+
+    if (question1 ==="1"){
+      $(".result1").show();
+    }
+
+
+
+
+    if (question1 ==="0"){
+      alert("Please fill out all questions");
+    }
+    if (question2 ==="0"){
+      alert("Please fill out all questions")
+    }
+    if (question3 ==="0"){
+      alert("Please fill out all questions")
+    }
+    if (question4 ==="0"){
+      alert("Please fill out all questions")
+    }
+    if (question5 ==="0"){
+      alert("Please fill out all questions")
+    }
 
     event.preventDefault();
   })
