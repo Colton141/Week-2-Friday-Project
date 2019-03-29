b1e3c9c05dc5fc85eb32b5aa024f7abf7b6d0f53
colton lacey <coltnutmiles14@gmail.com>
2019-03-29 15:57:41 -0700

    clean up javascript

--- js/scripts.js
+++ js/scripts.js
@@ -6,28 +6,15 @@ $(document).ready(function(){
     var question4 = $("#question4").val();
     var question5 = $("#question5").val();
 
-    if (question1 ==="1"){
+    if (question1 ==="1" && question2 === "3"){
+      $(".result").show();
+      $(".result2").show();
+    }
+    if (question1 ==="1" && question2 ==="1"){
+      $(".result").show();
       $(".result1").show();
     }
 
-
-
-
-    if (question1 ==="0"){
-      alert("Please fill out all questions");
-    }
-    if (question2 ==="0"){
-      alert("Please fill out all questions")
-    }
-    if (question3 ==="0"){
-      alert("Please fill out all questions")
-    }
-    if (question4 ==="0"){
-      alert("Please fill out all questions")
-    }
-    if (question5 ==="0"){
-      alert("Please fill out all questions")
-    }
 
     event.preventDefault();
   })
