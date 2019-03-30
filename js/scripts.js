60436ae49a40d4af85309737bb6a35485a5d3b3a
colton lacey <coltnutmiles14@gmail.com>
2019-03-29 19:32:11 -0700

    add more variations the survey resopnses in js

--- js/scripts.js
+++ js/scripts.js
@@ -9,13 +9,51 @@ $(document).ready(function(){
     if (question1 ==="1" && question2 === "3"){
       $(".result").show();
       $(".result2").show();
+      $("#about").show();
     }
     if (question1 ==="1" && question2 ==="1"){
       $(".result").show();
       $(".result1").show();
+      $("#about").show();
+    }
+    if (question1 ==="3" && question2 ==="1"){
+      $(".result3").show();
+      $(".result").show();
+      $("#about").show();
+    }
+    if (question1 ==="3" && question2 ==="2"){
+      $(".result3").show();
+      $(".result").show();
+      $("#about").show();
+    }
+    if (question1 ==="3" && question2 ==="3"){
+      $(".result1").show();
+      $(".result").show();
+      $("#about").show();
+    }
+    if (question1 ==="2" && question2 ==="1"){
+      $(".result2").show();
+      $(".result").show();
+      $("#about").show();
+    }
+    if (question1 ==="2" && question2 ==="2"){
+      $(".result2").show();
+      $(".result").show();
+      $("#about").show();
+    }
+    if (question1 ==="2" && question2 ==="3"){
+      $(".result1").show();
+      $(".result").show();
+      $("#about").show();
+    }
+    if (question1 ==="1" && question2 ==="2"){
+      $(".result").show();
+      $(".result2").show();
+      $("#about").show();
     }
 
 
+
     event.preventDefault();
-  })
-})
+  });
+});
